909df721db24f02c2717c74ecc23f0037d4cc081
HOANGNBD <Duchoang020120@gmail.com>
2023-12-26 15:44:07 +0700

    init eslint-prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
-import viteLogo from "../../vite.svg";
-
 import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
 
 import "./App.css";
 
