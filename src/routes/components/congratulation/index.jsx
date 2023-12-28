51f5b46234816a7bfe11a4e4e98808d14469fd40
Man Nguyen <128272683+ChuPhongMCP@users.noreply.github.com>
2023-12-28 11:34:52 +0700

    task/rank-congratulations

---
+++ src/routes/components/congratulation/index.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Congratulation(props) {
+  const { topThree } = props;
+
+  return (
+    <div className="flex w-full items-start justify-center bg-neutral-1 bg-[url('/assets/images/background/phao-hoa.jpg')] bg-center bg-no-repeat pt-[3.81rem]">
+      <div className="grid w-[59.5rem] grid-cols-3 gap-[0.75rem] px-[0.5rem] lg:px-0">
+        <div className="col-span-1 flex max-w-[19.375rem] flex-col items-center justify-end gap-[1rem]">
+          <img
+            className="h-[5.5rem] w-[5.5rem] rounded-full"
+            src={topThree[1]?.avatar}
+            alt="avatar"
+          />
+
+          <div className="flex flex-col items-center justify-start pb-[0.75rem] text-center">
+            <h3 className="m-0 font-roboto text-[1.5rem] font-[500] leading-[2rem] text-neutral-0">
+              {topThree[1]?.name}
+            </h3>
+
+            <span className="font-roboto text-[0.875rem] font-[400] leading-[1.375rem] text-neutral-0">
+              {topThree[1]?.position}
+            </span>
+          </div>
+
+          <div className="flex min-h-[7.0625rem] w-full flex-shrink-0 items-center justify-center rounded-t-[0.75rem] bg-primary-1">
+            <span className="font-roboto text-[4rem] font-[700] leading-[2.875rem] text-neutral-1">
+              {topThree[1]?.rank}
+            </span>
+          </div>
+        </div>
+
+        <div className="col-span-1 flex max-h-[22.5rem] max-w-[19.375rem] flex-col items-center justify-start gap-[1rem]">
+          <img
+            className="h-[5.5rem] w-[5.5rem] rounded-full"
+            src={topThree[0]?.avatar}
+            alt="avatar"
+          />
+
+          <div className="flex flex-col items-center justify-start pb-[0.75rem] text-center">
+            <h3 className="m-0 font-roboto text-[1.5rem] font-[500] leading-[2rem] text-neutral-0">
+              {topThree[0]?.name}
+            </h3>
+
+            <span className="font-roboto text-[0.875rem] font-[400] leading-[1.375rem] text-neutral-0">
+              {topThree[0]?.position}
+            </span>
+          </div>
+
+          <div className="flex min-h-[10.875rem] w-full flex-shrink-0 items-center justify-center rounded-t-[0.75rem] bg-primary-1">
+            <span className="font-roboto text-[4rem] font-[700] leading-[2.875rem] text-neutral-1">
+              {topThree[0]?.rank}
+            </span>
+          </div>
+        </div>
+
+        <div className="col-span-1 flex max-w-[19.375rem] flex-col items-center justify-end gap-[1rem]">
+          <img
+            className="h-[5.5rem] w-[5.5rem] rounded-full"
+            src={topThree[2]?.avatar}
+            alt="avatar"
+          />
+
+          <div className="flex flex-col items-center justify-start pb-[0.75rem] text-center">
+            <h3 className="m-0 font-roboto text-[1.5rem] font-[500] leading-[2rem] text-neutral-0">
+              {topThree[2]?.name}
+            </h3>
+
+            <span className="font-roboto text-[0.875rem] font-[400] leading-[1.375rem] text-neutral-0">
+              {topThree[2]?.position}
+            </span>
+          </div>
+
+          <div className="flex min-h-[5rem] w-full flex-shrink-0 items-center justify-center rounded-t-[0.75rem] bg-primary-1">
+            <span className="font-roboto text-[4rem] font-[700] leading-[2.875rem] text-neutral-1">
+              {topThree[2]?.rank}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Congratulation;
+
+Congratulation.propTypes = {
+  topThree: PropTypes.instanceOf(Array).isRequired,
+};
--- src/routes/ranking/index.jsx
+++ src/routes/ranking/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { topThree } from "@/constants/data/dataRanking";
+
+import Congratulation from "../components/congratulation";
+
 function Ranking() {
-  return <section>Ranking</section>;
+  return (
+    <section className="flex flex-col items-center justify-start gap-[1.5rem]">
+      <Congratulation topThree={topThree} />
+    </section>
+  );
 }
 
 export default Ranking;
