5965e91a8aa512f637e4e2073a99de4c41d233c2
HOANGNBD <Duchoang020120@gmail.com>
2024-01-02 14:07:24 +0700

    task/header-layout-footer

--- src/utils/onCheckActiveLink.js
+++ src/utils/onCheckActiveLink.js
@@ -3,7 +3,7 @@ import classnames from "classnames";
 export function onCheckActiveLink(pathname, href) {
   const isActive = pathname === href || pathname === `${href}/`;
   return classnames(
-    "no-underline font-[0.875rem] hover:text-primary-2/40 duration-500",
+    "no-underline font-[0.875rem] hover:text-primary-2/40 duration-500 leading-[1.375rem]",
     {
       "text-primary-0": isActive,
       "text-black": !isActive,
