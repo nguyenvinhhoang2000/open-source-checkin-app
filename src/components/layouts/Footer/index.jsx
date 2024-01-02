c4325c37a3c75c0d718b8254334bfbd38399b307
HOANGNBD <Duchoang020120@gmail.com>
2024-01-02 13:41:20 +0700

    task/header-layout-footer

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-mainBg">
-      <div className="container flex flex-row items-center justify-between bg-mainBg py-[1.1875rem] text-black/25">
+      <div className="container flex flex-row items-center justify-between py-[1.1875rem] text-black/25">
         <span>2023 © Wiicamp</span>
         <span>Design & Develop by Wiicamp</span>
       </div>
