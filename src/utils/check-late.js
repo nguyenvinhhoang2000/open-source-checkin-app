28436e57aa1fc12e5b3309082d3bf89ead21a48c
HOANGNBD <Duchoang020120@gmail.com>
2024-01-09 15:52:14 +0700

    task/dashboard-table

--- src/utils/check-late.js
+++ src/utils/check-late.js
@@ -3,23 +3,12 @@ import dayjs from "dayjs";
 import { TIME_WORKING } from "@/constants/time-working";
 
 export default function checkLate(text, type) {
-  const currentTime = dayjs(text);
   const checkTime = dayjs(
     `${dayjs(text).format("YYYY-MM-DD")} ${TIME_WORKING[type].time}`,
   );
-  const isLate = currentTime.isAfter(checkTime);
-  switch (type) {
-    case "in":
-      return {
-        isLate,
-        type: isLate ? TIME_WORKING[type].late : TIME_WORKING[type].early,
-      };
-    case "out":
-      return {
-        isLate,
-        type: isLate ? TIME_WORKING[type].late : TIME_WORKING[type].early,
-      };
-    default:
-      return { error: "Unknown type" };
-  }
+  const isLate = dayjs(text).isAfter(checkTime);
+  return {
+    isLate,
+    type: isLate ? TIME_WORKING[type].late : TIME_WORKING[type].early,
+  };
 }
