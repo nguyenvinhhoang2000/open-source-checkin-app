135b7bd1f9bb17867a24f1e13bd56945ff232035
HOANGNBD <Duchoang020120@gmail.com>
2024-01-10 09:42:08 +0700

    task/edit-profile

--- src/routes/dashboard/components/dashboard-edit-profile/modal-edit/index.jsx
+++ src/routes/dashboard/components/dashboard-edit-profile/modal-edit/index.jsx
@@ -57,7 +57,7 @@ function ModalEditProfile({
       }
     >
       <Form
-        className="border-t border-t-black/5 px-4 pt-4"
+        className="border-b border-t border-b-black/5 border-t-black/5 px-4 pb-1 pt-4"
         form={updateProfileForm}
         layout="vertical"
         name="edit-profile"
