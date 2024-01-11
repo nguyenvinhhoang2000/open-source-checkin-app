6fdf7cc3da4fadba8056f5a084e29db473003117
HOANGNBD <Duchoang020120@gmail.com>
2024-01-11 10:09:25 +0700

    fix-time

--- src/constants/time-working.js
+++ src/constants/time-working.js
@@ -1,11 +1,11 @@
 export const TIME_WORKING = {
   in: {
-    time: "08:30:00",
+    time: new Date("08:30:00"),
     early: "Check-in Early",
     late: "Check-in Late",
   },
   out: {
-    time: "17:30:00",
+    time: new Date("17:30:00"),
     early: "Check-out Early",
     late: "Check-out Late",
   },
