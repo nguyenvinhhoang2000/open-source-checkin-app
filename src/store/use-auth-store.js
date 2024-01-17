fd17f42cfefa5a74c8e5a9a40ac6f3d3e5a68922
HOANGNBD <Duchoang020120@gmail.com>
2024-01-17 11:06:01 +0700

    fix-API_URL

--- src/store/use-auth-store.js
+++ src/store/use-auth-store.js
@@ -6,46 +6,41 @@ import { LOCATIONS } from "@/constants/routes";
 import { TYPE_MESSAGE } from "@/constants/type-message";
 import axiosClient from "@/services/memberApi";
 
-const useAuthStore = create(
-  (set) => ({
-    user: null,
-    onLogin: async (data) => {
-      try {
-        const {
-          data: { payload: token, message },
-        } = await axiosClient.post(`/client/auth/login`, data);
+const useAuthStore = create((set) => ({
+  user: null,
+  onLogin: async (data) => {
+    try {
+      const {
+        data: { payload: token, message },
+      } = await axiosClient.post(`/client/auth/login`, data);
 
-        cookie.save("token", token, {
-          path: LOCATIONS.LOGIN,
-          // EX: One day
-          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        });
-        return {
-          ok: true,
-          status: TYPE_MESSAGE.SUCCESS,
-          message,
-        };
-      } catch (error) {
-        return {
-          ok: false,
-          status: TYPE_MESSAGE.ERROR,
-          message: error.response ? error.response.data.message : "Sytem error",
-        };
-      }
-    },
-    onLogout: async () => {
-      cookie.remove(COOKIES_KEYS.TOKEN, { path: LOCATIONS.LOGIN });
-      window.location.replace(LOCATIONS.LOGIN);
-
-      set({ user: null, token: null });
-    },
-    onGetUserInfo: async (user) => {
-      set({ user });
-    },
-  }),
-  {
-    name: "auth-storage",
+      cookie.save("token", token, {
+        path: LOCATIONS.LOGIN,
+        // EX: One day
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      });
+      return {
+        ok: true,
+        status: TYPE_MESSAGE.SUCCESS,
+        message,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        status: TYPE_MESSAGE.ERROR,
+        message: error.response ? error.response.data.message : "Sytem error",
+      };
+    }
   },
-);
+  onLogout: async () => {
+    cookie.remove(COOKIES_KEYS.TOKEN, { path: LOCATIONS.LOGIN });
+    window.location.replace(LOCATIONS.LOGIN);
+
+    set({ user: null, token: null });
+  },
+  onGetUserInfo: async (user) => {
+    set({ user });
+  },
+}));
 
 export { useAuthStore };
