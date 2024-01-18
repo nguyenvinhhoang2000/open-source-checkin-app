2d5515873361369c0439efbb7d51393ba1b1fdba
HOANGNBD <Duchoang020120@gmail.com>
2024-01-18 15:29:48 +0700

    fix: render login page when user already exist

--- src/components/auth-route/index.jsx
+++ src/components/auth-route/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { Navigate, useSearchParams } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import AppLoadingPage from "@/components/apps/app-page-loading";
@@ -9,19 +9,15 @@ import useAppMounted from "@/store/use-app-mounted";
 import useAuthStore from "@/store/use-auth-store";
 
 function AuthRoute({ children }) {
-  const navigate = useNavigate();
-
   const [searchParams] = useSearchParams();
   const redirect = searchParams.get("redirect");
 
   const user = useAuthStore().user;
   const isAppMounted = useAppMounted().isAppMounted;
 
-  React.useEffect(() => {
-    if (user) {
-      navigate(redirect || LOCATIONS.MEMBER_DASHBOARD);
-    }
-  }, [user]); // eslint-disable-line
+  if (user) {
+    return <Navigate to={redirect || LOCATIONS.MEMBER_DASHBOARD} />;
+  }
 
   if (!user && !isAppMounted) {
     return <AppLoadingPage />;
