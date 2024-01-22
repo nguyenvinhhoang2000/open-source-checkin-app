ce298a9f3db3bc43da344b2e0be3fbd204a503cb
HOANGNBD <Duchoang020120@gmail.com>
2024-01-22 15:18:43 +0700

    update: use air bnb hook instead using react-hooks/recommend

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,8 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:import/recommended",
     "plugin:prettier/recommended",
-    "plugin:react-hooks/recommended",
+    // "plugin:react-hooks/recommended",
+    "airbnb/hooks",
     "prettier",
   ],
   overrides: [
