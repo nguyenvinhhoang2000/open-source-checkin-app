0e4829a8f2d5586e7e10b2ba69231ab317a86c73
HOANGNBD <Duchoang020120@gmail.com>
2024-01-23 17:19:11 +0700

    update: function check allowance edit, can edit in day current

--- src/utils/check-allowce-edit-absent.js
+++ src/utils/check-allowce-edit-absent.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
 
 export default function onCheckIsEditAbsent(record) {
-  return dayjs(new Date()).isBefore(dayjs(record));
+  return dayjs(new Date()).subtract(1, "day").isBefore(dayjs(record));
 }
