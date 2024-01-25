59df27a8c53116cf4827d05d8b0ef7c1a96bf70a
HOANGNBD <Duchoang020120@gmail.com>
2024-01-25 09:29:26 +0700

    edit: condition to render modal view

--- src/routes/absent-request/components/absent-view/index.jsx
+++ src/routes/absent-request/components/absent-view/index.jsx
@@ -6,28 +6,27 @@ import ABSENT_TABLE_COLUMNS from "@/constants/absent-table";
 import { FORMAT_DATE } from "@/constants/format-date";
 import { emptyObj } from "@/utils/empty-types";
 
-function AbsentView({ isModalOpen, currentData }) {
+function AbsentView({ currentData }) {
   return (
     <div className="flex flex-col gap-2 border-b border-t border-b-black/5 border-t-black/5 px-6 pb-1 pt-4">
-      {isModalOpen &&
-        ABSENT_TABLE_COLUMNS.map(
-          (item, index) =>
-            index < 4 && (
-              <div
-                key={item.key}
-                className="flex flex-row flex-wrap justify-between gap-2 font-roboto"
-              >
-                <span className="font-bold">{item.title}</span>
-                <span>
-                  {item.key === ABSENT_TABLE_COLUMNS[3].key
-                    ? currentData[item.key]?.toString()
-                    : dayjs(new Date(currentData[item.key])).format(
-                        FORMAT_DATE.FORMAT_DATE_FOR_DATE_AND_12_HOURS_AND_INTERVALS,
-                      )}
-                </span>
-              </div>
-            ),
-        )}
+      {ABSENT_TABLE_COLUMNS.map(
+        (item, index) =>
+          index < 4 && (
+            <div
+              key={item.key}
+              className="flex flex-row flex-wrap justify-between gap-2 font-roboto"
+            >
+              <span className="font-bold">{item.title}</span>
+              <span>
+                {item.key === ABSENT_TABLE_COLUMNS[3].key
+                  ? currentData[item.key]?.toString()
+                  : dayjs(new Date(currentData[item.key])).format(
+                      FORMAT_DATE.FORMAT_DATE_FOR_DATE_AND_12_HOURS_AND_INTERVALS,
+                    )}
+              </span>
+            </div>
+          ),
+      )}
     </div>
   );
 }
@@ -35,11 +34,9 @@ function AbsentView({ isModalOpen, currentData }) {
 export default React.memo(AbsentView);
 
 AbsentView.propTypes = {
-  isModalOpen: PropTypes.bool,
   currentData: PropTypes.instanceOf(Object),
 };
 
 AbsentView.defaultProps = {
-  isModalOpen: false,
   currentData: emptyObj,
 };
